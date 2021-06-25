8e814f18cfa63353d11b2fcdec2d7a38763cbcd0
gutakeda <gtaked@hotmail.com>
2021-06-24 23:32:38 -0300

    test githubactions deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,7 +52,7 @@ export function Home() {
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+                <p>Tire as dúvidas da sua audiência em tempo-real!</p>
             </aside>
             <main>
                 <div className="main-content">
